2c441ac1a8cd91287da7f25f61f18a19477bc533
mrichar5-code <mrichar5@ucsc.edu>
2026-05-06 15:53:03 -0700

    vhjb jn

---
+++ garden/creature.js
@@ -0,0 +1,18 @@
+// add creature to the garden
+$("#crAdd").click( function(){
+
+    // grab the value from the text input and assign it to a variable crName
+    let crName=$("#crName").val();
+    // test in console
+    console.log(crName);  
+
+    if( crName.length > 2) { 
+        // add the name to a div with a bit of html code attached
+        $("#creature-list").append("<div>"+crName+"</div>");
+    }
+   
+
+     $("#crName").val(""); // write the value
+    // $("#crName").val(); // retrieve the value
+
+}); 
