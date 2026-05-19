0abaac8120352b392f6aabf9d5ed93cb90c9e9bf
mrichar5-code <mrichar5@ucsc.edu>
2026-05-18 21:31:23 -0700

    ghj

--- garden/creature.js
+++ garden/creature.js
@@ -3,15 +3,33 @@ $("#crAdd").click( function(){
 
     // grab the value from the text input and assign it to a variable crName
     let crName=$("#crName").val();
+    let crColor=$("#crColor").val();
+    let crEyesNum=$("#crEyesNum").val();
+    // lets construct html for eyes
+    let crEyesHtml="";
+    for (let i = 0; i < crEyesNum; i++) {
+        crEyesHtml=crEyesHtml+ "<div class='eye'>.</div>";
+    }
+
+
     // test in console
     console.log(crName);  
+    console.log(crColor);  
+    console.log(crEyesNum);  
+    console.log(crEyesHtml); 
 
     if( crName.length > 2) { 
-        // add the name to a div with a bit of html code attached
-        $("#creature-list").append("<div>"+crName+"</div>");
-    }
+       $("#creature-list").append(`
+            <div class="creature">
+                <div class="creature-body" style="background-color: ${crColor}"> ${crEyesHtml} </div>
+                <div class="creature-info">${crName}</div>
+            </div>
+        `);
+       }
+            
+    
    
-
+    // "<div>"+crName+crColor+crEyesNum+"</div>");
      $("#crName").val(""); // write the value
     // $("#crName").val(); // retrieve the value
 
